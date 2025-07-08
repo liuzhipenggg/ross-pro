b03ba0c99b69326f5ae23457a2fa432e1b4ce372
Haochen-Wang-409 <1137810041@qq.com>
2025-07-08 14:08:55 +0800

    update

--- VLMEvalKit/eval.sh
+++ VLMEvalKit/eval.sh
@@ -4,7 +4,8 @@ export http_proxy=agent.baidu.com:8188
 export https_proxy=agent.baidu.com:8188
 export no_proxy=baidu.com,baidubce.com,localhost,127.0.0.1,bj.bcebos.com
 
-torchrun --nproc-per-node=8 \
+torchrun --nproc-per-node=8 --master-addr=localhost --master-port=5123 \
     run.py \
     --data POPE HallusionBench MMBench_DEV_EN MMBench_DEV_CN SEEDBench_IMG OCRBench MMVP ChartQA RealWorldQA \
-    --model 
+    --model ross-siglip-qwen2-7b-flux-kl8-dit3x-pt558k-sft737k llava-siglip-qwen2-7b-pt558k-sft737k
+
