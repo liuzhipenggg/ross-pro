fe8142f1ac3bf8b527e8420ad3bfffd807e470fc
Haochen-Wang-409 <1137810041@qq.com>
2025-07-09 13:55:40 +0800

    update

--- VLMEvalKit/eval.sh
+++ VLMEvalKit/eval.sh
@@ -6,6 +6,10 @@ export no_proxy=baidu.com,baidubce.com,localhost,127.0.0.1,bj.bcebos.com
 
 set -x
 
+# python3 run.py \
+#     --data POPE HallusionBench MMBench_DEV_EN MMBench_DEV_CN SEEDBench_IMG OCRBench MMVP ChartQA_TEST RealWorldQA \
+#     --model llava-siglip-qwen2-7b-pt558k-sft737k ross-siglip-qwen2-7b-flux-kl8-dit3x-pt558k-sft737k
+
 python3 run.py \
-    --data POPE HallusionBench MMBench_DEV_EN MMBench_DEV_CN SEEDBench_IMG OCRBench MMVP ChartQA_TEST RealWorldQA \
-    --model llava-siglip-qwen2-7b-pt558k-sft737k ross-siglip-qwen2-7b-flux-kl8-dit3x-pt558k-sft737k
+    --data MMBench_DEV_EN \
+    --model llava-siglip-qwen2-7b-pt558k-sft737k
--- scripts/train_ross_pro/finetune_siglip_qwen2_sd14.sh
+++ scripts/train_ross_pro/finetune_siglip_qwen2_sd14.sh
@@ -4,7 +4,7 @@ export http_proxy=agent.baidu.com:8188
 export https_proxy=agent.baidu.com:8188
 export no_proxy=baidu.com,baidubce.com,localhost,127.0.0.1,bj.bcebos.com
 
-EXP_NAME="ross-pro-siglip-qwen2-7b-flux-kl8-dit3x-pt558k-sft737k"
+EXP_NAME="ross-pro-siglip-qwen2-7b-sd14-kl8-mlp2x-pt558k-sft737k"
 export WANDB_PROJECT=ross-pro
 
 set -x
@@ -20,18 +20,18 @@ torchrun --nproc-per-node=8 --nnodes $1 --node_rank $2 \
     \
     --deepspeed ./scripts/zero3.json \
     --model_name_or_path /mnt/haochen/hf_home/Qwen2-7B-Instruct \
-    --pretrain_mm_mlp_adapter ./checkpoints/ross-siglip-qwen2-7b-flux-kl8-dit3x-pt558k/mm_projector.bin \
-    --pretrain_mm_inv_mlp_adapter ./checkpoints/ross-siglip-qwen2-7b-flux-kl8-dit3x-pt558k/mm_inv_projector.bin \
+    --pretrain_mm_mlp_adapter ./checkpoints/ross-pro-siglip-qwen2-7b-sd14-kl8-mlp2x-pt558k/mm_projector.bin \
+    --pretrain_mm_inv_mlp_adapter ./checkpoints/ross-pro-siglip-qwen2-7b-sd14-kl8-mlp2x-pt558k/mm_inv_projector.bin \
     --output_dir ./checkpoints/$EXP_NAME \
     --vision_tower /mnt/haochen/hf_home/siglip-so400m-patch14-384 \
     --version qwen_2 \
-    --mm_pixel_decoder /mnt/haochen/hf_home/FLUX.1-dev/vae \
+    --mm_pixel_decoder /root/paddlejob/stable-diffusion-v1-4/vae \
     \
     --data_path /mnt/haochen/datasets/Cambrian-737K/Cambrian737k/Cambrian737k.json \
     --image_folder /mnt/haochen/datasets/Cambrian-737K/Cambrian737k \
     \
     --mm_projector_type mlp2x_gelu \
-    --mm_inv_projector_type denoiser_vit3x \
+    --mm_inv_projector_type sd14_mlp2x \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
