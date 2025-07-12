5d929fe9b3eb9753a6c35460088dbf8756e62c9f
Haochen-Wang-409 <1137810041@qq.com>
2025-07-12 17:16:10 +0800

    update

--- VLMEvalKit/eval.sh
+++ VLMEvalKit/eval.sh
@@ -6,10 +6,6 @@ export no_proxy=baidu.com,baidubce.com,localhost,127.0.0.1,bj.bcebos.com
 
 set -x
 
-# python3 run.py \
-#     --data POPE HallusionBench MMBench_DEV_EN MMBench_DEV_CN SEEDBench_IMG OCRBench MMVP ChartQA_TEST RealWorldQA \
-#     --model llava-siglip-qwen2-7b-pt558k-sft737k ross-siglip-qwen2-7b-flux-kl8-dit3x-pt558k-sft737k
-
-python3 run.py \
-    --data MMBench_DEV_EN \
-    --model llava-siglip-qwen2-7b-pt558k-sft737k
+torchrun --nproc-per-node=8 --master-port=8591 run.py \
+    --data POPE HallusionBench MMBench_DEV_EN MMBench_DEV_CN SEEDBench_IMG OCRBench MMVP ChartQA_TEST RealWorldQA \
+    --model ross-pro-siglip-qwen2-7b-sd14-kl8-mlp2x-pt558k-sft737k ross-pro-siglip-qwen2-7b-sd15-kl8-mlp2x-pt558k-sft737k ross-pro-siglip-qwen2-7b-sd21-kl8-mlp2x-pt558k-sft737k ross-pro-siglip-qwen2-7b-sd15-kl8-mlp2x-pt558k-sft737k-ftcrossattn
